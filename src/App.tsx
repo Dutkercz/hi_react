f53ab7b4be2fef027d7061780b51702aa826e158
Dutkercz <dutkercz@gmail.com>
2026-08-17 15:34:39 -0300

    feat: update package.json and package-lock.json with new dependencies; refactor App component to include routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import HomePage from "./pages/home-page"
+import { NavBar } from "./components/navbar/nav-bar"
+import { Route, Routes } from "react-router-dom"
+import { Toaster } from "sonner"
+import OccupationPage from "./pages/occupation-page"
+import RegisterPage from "./pages/register-page"
 
-export function App() {
+const client = new QueryClient()
+
+const App = () => {
+
   return (
-    <h1>ola</h1>
+    <QueryClientProvider client={client}>
+      <Toaster />
+      <header>
+        <NavBar />
+      </header>
+
+      <Routes>
+
+        <Route path="/" element={<HomePage />} />
+        <Route path="/bookings" element={<OccupationPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+
+      </Routes>
+
+
+
+    </QueryClientProvider>
+
   )
+
 }
 
 export default App
